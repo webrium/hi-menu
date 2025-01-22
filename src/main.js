f2e8dc8d161771c8960d1f520c224370e32a6a0b
parsgit <nvioir@gmail.com>
2025-01-22 20:15:49 +0000

    improve responsive

--- src/main.js
+++ src/main.js
@@ -4,17 +4,25 @@ import svg_chevron_right from "./chevron_right.svg";
 export default class ContextMenu {
   longpress_event = false;
 
-  constructor(elementID, contextmenu_callback=null) {
+  constructor(elementID, contextmenu_callback = null) {
     this.menu = document.createElement("div");
     this.menu.id = "custom-menu";
     this.menu.setAttribute(
       "class",
-      "hi-select-none hi-bg-white dark:hi-bg-slate-800 dark:hi-text-white hi-absolute hi-flex hi-flex-col hi-gap-2 hi-shadow-md hi-shadow-gray-400 dark:hi-shadow-gray-100/50 hi-p-2 hi-z-[1000] hi-rounded-lg"
+      "hi-select-none hi-bg-white dark:hi-bg-slate-800 dark:hi-text-white hi-absolute hi-flex hi-flex-col hi-gap-2 hi-shadow-md hi-shadow-gray-400 dark:hi-shadow-gray-100/50 hi-p-4 hi-z-[1000] hi-rounded-lg hi-w-[90%] hi-max-w-[300px]"
     );
-    this.menu.style.maxWidth = "300px";
-
     this.menu.style.display = "none";
 
+    // Add a semi-transparent backdrop for modal effect
+    this.backdrop = document.createElement("div");
+    this.backdrop.setAttribute(
+      "class",
+      "hi-fixed hi-inset-0 hi-bg-black/50 hi-z-[999]"
+    );
+    this.backdrop.style.display = "none";
+    this.backdrop.onclick = () => this.hideMenu();
+
+    document.body.appendChild(this.backdrop);
     document.body.appendChild(this.menu);
 
     this.elements = [document];
@@ -29,13 +37,23 @@ export default class ContextMenu {
       this.event = event;
       this.target = target;
 
-      
-      this.menu.style.left = `${event.pageX}px`;
-      this.menu.style.top = `${event.pageY}px`;
+      if (window.innerWidth <= 768) {
+        // Center the menu for mobile devices
+        this.menu.style.left = "50%";
+        this.menu.style.top = "50%";
+        this.menu.style.transform = "translate(-50%, -50%)";
+        this.backdrop.style.display = "block";
+      } else {
+        // Show the menu at the cursor's position for larger screens
+        this.menu.style.left = `${event.pageX}px`;
+        this.menu.style.top = `${event.pageY}px`;
+        this.menu.style.transform = "none";
+      }
+
       this.menu.style.display = "block";
-      
-      if(typeof contextmenu_callback ==='function'){
-        contextmenu_callback(event)
+
+      if (typeof contextmenu_callback === "function") {
+        contextmenu_callback(event);
       }
     };
 
@@ -43,56 +61,14 @@ export default class ContextMenu {
       element.addEventListener("contextmenu", event)
     );
 
-    if (this.longpress_event) {
-      
-      let pressTimer;
-
-      // Duration for long press (in milliseconds)
-      const longPressDuration = 500;
-
-      // Function to execute on long press
-      const onLongPress = () => {
-        event();
-      };
-
-      // Touchstart event
-      button.addEventListener("touchstart", (e) => {
-        e.preventDefault();
-        pressTimer = setTimeout(onLongPress, longPressDuration);
-      });
-
-      // Touchend event
-      button.addEventListener("touchend", (e) => {
-        e.preventDefault();
-        clearTimeout(pressTimer);
-      });
-
-      // Cancel the timer if the user moves their finger
-      button.addEventListener("touchmove", (e) => {
-        e.preventDefault();
-        clearTimeout(pressTimer);
-      });
-
-      // Optional: Add mouse support for desktop
-      button.addEventListener("mousedown", (e) => {
-        e.preventDefault();
-        pressTimer = setTimeout(onLongPress, longPressDuration);
-      });
-
-      button.addEventListener("mouseup", (e) => {
-        e.preventDefault();
-        clearTimeout(pressTimer);
-      });
-
-      button.addEventListener("mouseleave", (e) => {
-        e.preventDefault();
-        clearTimeout(pressTimer);
-      });
-    }
-
     document.addEventListener("click", () => {
-      this.menu.style.display = "none";
+      this.hideMenu();
     });
+  }
+
+  hideMenu() {
+    this.menu.style.display = "none";
+    this.backdrop.style.display = "none";
   }
 
   setLongpressEvent() {
@@ -103,22 +79,20 @@ export default class ContextMenu {
     this.elements.forEach(event);
   }
 
-
-  onFocus(callback){
-    this.setToElement(element=>{
-      element.addEventListener('focus', event=>callback(event))
-    })
+  onFocus(callback) {
+    this.setToElement((element) => {
+      element.addEventListener("focus", (event) => callback(event));
+    });
   }
 
-  onBlur(callback){
-    this.setToElement(element=>{
-      element.addEventListener('blur', event=>callback(event))
-    })
+  onBlur(callback) {
+    this.setToElement((element) => {
+      element.addEventListener("blur", (event) => callback(event));
+    });
   }
 
   title(title, textAlign = "center") {
     const find = this.menu.querySelector("div[data-title]");
-    console.log(find);
 
     if (find == null) {
       const div = document.createElement("div");
@@ -192,49 +166,9 @@ export default class ContextMenu {
     if (action) {
       item.onclick = (e) => {
         e.stopPropagation();
-        this.menu.style.display = "none";
+        this.hideMenu();
         action(this.event);
       };
-    }
-
-    if (submenuItems) {
-      console.log();
-
-      item.appendChild(
-        this.makeSvgFromData(
-          svg_chevron_right,
-          "hi-size-4 hi-text-black dark:hi-text-white"
-        )
-      );
-
-      const submenu = document.createElement("div");
-      submenu.style.position = "absolute";
-      submenu.style.top = "0";
-      submenu.style.left = "100%";
-      submenu.style.display = "none";
-      submenu.setAttribute(
-        "class",
-        "hi-bg-white dark:hi-bg-slate-800 dark:hi-text-white hi-absolute hi-flex hi-flex-col hi-gap-2 hi-shadow-md hi-shadow-gray-400 hi-p-2 hi-z-[1000] hi-rounded-lg"
-      );
-
-      submenuItems.forEach(
-        ({ label: subLabel, action: subAction, submenu: subSubmenu }) => {
-          this.add(subLabel, subAction, subSubmenu).forEach((subItem) => {
-            submenu.appendChild(subItem);
-          });
-        }
-      );
-
-      item.onmouseenter = () => {
-        submenu.style.display = "block";
-      };
-      item.onmouseleave = () => {
-        submenu.style.display = "none";
-      };
-
-      item.appendChild(submenu);
-    } else {
-      item.classList.add("hi-cursor-pointer");
     }
 
     this.menu.appendChild(item);
